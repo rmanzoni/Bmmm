ac87a942dc06566ea28fd649a471825aab5aff36
Riccardo Manzoni <riccardo.manzoni@cern.ch>
2024-02-05 14:07:15 +0100

    add prod tools

--- Bmmm/Analysis/src/classes.h
+++ Bmmm/Analysis/src/classes.h
@@ -5,10 +5,12 @@
 #include "DataFormats/Common/interface/Wrapper.h"
 #include "Bmmm/Analysis/interface/KVFitter.h"
 #include "Bmmm/Analysis/interface/RDsKinVtxFitter.h"
+#include "Bmmm/Analysis/interface/B4MuKinVtxFitter.h"
 
 namespace {
   struct RDs {
     KVFitter KalVtx_;
     RDsKinVtxFitter KinVtx_;
+    B4MuKinVtxFitter BKinVtx_;
   };
 }
